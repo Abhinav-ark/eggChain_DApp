3b395f95a6593e6ec89e05bc523f379aa2a0ddfc
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-04-29 22:38:55 +0530

    smart contract - create Shipment function done

---
+++ contracts/Tracking.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Tracking {
+    enum ShipmentStatus {PENDING, INTRANSIT, DELIVERED}
+
+    struct Shipment {
+        address sender;
+        address receiver;
+        uint256 pickupTime;
+        uint256 deliveryTime;
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        bool isPaid;
+    }
+
+    mapping(address => Shipment[]) public shipments;
+
+    uint256 public shipmentCount;
+
+    //another struct for display purpose
+    struct TypeShipment {
+        address sender;
+        address receiver;
+        uint256 pickupTime;
+        uint256 deliveryTime;
+        uint256 distance;
+        uint256 price;
+        ShipmentStatus status;
+        bool isPaid;
+    }
+
+    TypeShipment[] typeShipments; 
+
+    event ShipmentCreated(address indexed sender, address indexed receiver, uint256 pickupTime, uint256 distance, uint256 price);
+    event ShipmentInTransit(address indexed sender, address indexed receiver, uint256 pickupTime);
+    event ShipmentDelivered(address indexed sender, address indexed receiver, uint256 deliveryTime);
+    event ShipmentPaid(address indexed sender, address indexed receiver, uint256 price);
+
+
+    constructor() {
+        shipmentCount = 0;
+    }
+
+    function createShipment(address _receiver, uint256 _pickupTime, uint256 _distance, uint256 _price) public payable {
+        require(msg.value == _price, "Payment must be equal to price");
+
+        Shipment memory shipment = Shipment(msg.sender, _receiver, _pickupTime, 0, _distance, _price, ShipmentStatus.PENDING, false);
+        
+        shipments[msg.sender].push(shipment);
+        shipmentCount++;
+
+        typeShipments.push(TypeShipment(msg.sender, _receiver, _pickupTime, 0, _distance, _price, ShipmentStatus.PENDING, false));
+
+        emit ShipmentCreated(msg.sender, _receiver, _pickupTime, _distance, _price);
+    }
+}
